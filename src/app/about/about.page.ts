7fee18069e80cd934e6664ee343b92cc0c2bde20
amirulasrof <acop95@gmail.com>
2018-08-08 02:40:51 +0800

    first commit

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage {
+
+	constructor(public alertCtrl: AlertController) {}
+
+	async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
